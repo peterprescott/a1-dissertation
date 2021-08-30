bbc83c80b12b6178a15dfa9eb690f413613f7a0a
Peter Prescott <prescott.peter@gmail.com>
2021-08-30 18:03:45 +0100

    Add method for ST_Contains query

--- code/nbhd/data.py
+++ code/nbhd/data.py
@@ -10,11 +10,14 @@ pwd = os.environ.get('DB_PASSWORD')
 host = os.environ.get('DB_HOSTNAME')
 port = os.environ.get('DB_PORT')
 
+# note that 27700 is the British National Grid reference number.
+
 
 class Base():
     'Hide the database code.'
     
     __url = f"postgres+psycopg2://{user}:{pwd}@{host}:{port}/geodemo"
+    print('Initializing database connection...')
     engine = create_engine(__url)
 
     def ls(self):
@@ -46,7 +49,16 @@ class Base():
             ST_SetSRID(ST_Point({x}, {y}), 27700), 
             {buffer})
         '''
-        return gpd.read_postgis(sql, self.engine, geom_col='geometry')
+#         return gpd.read_postgis(sql, self.engine, geom_col='geometry')
+        return self.query(sql)
         
+    def contains(self, table, polygon):
+        sql = f'''
+        SELECT
+        * from {table}
+        WHERE 
+            ST_Contains(ST_GeomFromText('{polygon}', 27700), geometry); 
+        '''
+        return self.query(sql)
         
    
