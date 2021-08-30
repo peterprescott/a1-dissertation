16e6fab99023f31dc22974e07876bffbcd0a6c41
Peter Prescott <prescott.peter@gmail.com>
2021-08-30 16:27:59 +0100

    rename file

--- code/nbhd/data.py
+++ code/nbhd/data.py
@@ -30,10 +30,23 @@ class Base():
     def query(self, sql, spatial=False):
         'Query database.'
         
+        if 'ST_' in sql:
+            spatial = True
+        
         if not spatial:
             return pd.read_sql(sql, self.engine)
         else:
-            return gpd.read_postgis(sql, self.engine)
+            return gpd.read_postgis(sql, self.engine, geom_col='geometry')
+    
+    def within(self, table, buffer, x=338157, y=393037):
+        sql = f'''
+        SELECT * FROM {table} 
+        WHERE ST_DWithin(
+            geometry, 
+            ST_SetSRID(ST_Point({x}, {y}), 27700), 
+            {buffer})
+        '''
+        return gpd.read_postgis(sql, self.engine, geom_col='geometry')
         
         
    
